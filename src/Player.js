174592170d17481ba26a6c7e61ed95b36149d8ba
IsakA <ial0705@student.skelleftea.se>
2025-11-20 14:31:54 +0100

    Tjo bre

--- src/Player.js
+++ src/Player.js
@@ -6,9 +6,37 @@ export default class Player {
     this.y = y;
     this.w = w;
     this.h = h;
+    this.r = 100;
+    this.g = 100;
+    this.b = 100;
+
+    this.gravid = 0.03;
+    this.y_vel = 0;
+    this.x_vel = 0;
+
     this.hitbox = new HitBox(x, y, 0, 0, w, h);
   }
+
+  setColor(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+  }
+
   draw() {
+    fill(this.r, this.g, this.b);
     rect(this.x, this.y, this.w, this.h);
+  }
+
+  update(dT, ground) {
+    this.y_vel += this.gravid;
+
+    if (this.hitbox.intersectsWith(ground.hitbox)) {
+      this.y_vel = 0;
+      this.y = ground.y - this.h;
+    }
+
+    this.y += this.y_vel * dT;
+    this.hitbox.moveTo(this.x, this.y);
   }
 }
--- src/sketch.js
+++ src/sketch.js
@@ -9,18 +9,28 @@ let ground;
 
 let player;
 
+let deltaTime = 5;
+let lastTime;
+
 sketch.setup = () => {
   createCanvas(800, 600);
   blobbe = new Block(200, 200, 200, 50);
   blobbe.setColor(100, 100, 2);
   ground = new Block(0, height - 100, width, 100, 150, 200, 0);
 
-  player = new Player(200, 200, 50, 50);
+  player = new Player(100, 300, 50, 50);
+  player.setColor(255, 0, 0);
+  lastTime = millis();
 };
 
 sketch.draw = () => {
+  deltaTime = millis() - lastTime;
+
   background(0, 100, 200);
   ground.draw();
   blobbe.draw();
   player.draw();
+
+  player.update(deltaTime, ground);
+  lastTime = millis();
 };
