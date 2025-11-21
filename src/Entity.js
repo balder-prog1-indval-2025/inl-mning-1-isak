16dabe8c074b00ec8538747a84a64cd47b26126e
rsten <ruben.stenlund@gmail.com>
2025-11-21 08:25:07 +0100

    Entity parent class structure

---
+++ src/Entity.js
@@ -0,0 +1,28 @@
+export default class Entity {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.r = 100;
+    this.g = 100;
+    this.b = 100;
+
+    this.gravid = 0.03;
+    this.y_vel = 0;
+    this.x_vel = 0;
+
+    this.hitbox = new HitBox(x, y, 0, 0, w, h);
+  }
+
+  setColor(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+  }
+
+  draw() {
+    fill(this.r, this.g, this.b);
+    rect(this.x, this.y, this.w, this.h);
+  }
+}
--- src/Player.js
+++ src/Player.js
@@ -1,31 +1,9 @@
 import HitBox from "./Box";
+import Entity from "./Entity";
 
-export default class Player {
+export default class Player extends Entity {
   constructor(x, y, w, h) {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.r = 100;
-    this.g = 100;
-    this.b = 100;
-
-    this.gravid = 0.03;
-    this.y_vel = 0;
-    this.x_vel = 0;
-
-    this.hitbox = new HitBox(x, y, 0, 0, w, h);
-  }
-
-  setColor(r, g, b) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
-  }
-
-  draw() {
-    fill(this.r, this.g, this.b);
-    rect(this.x, this.y, this.w, this.h);
+    super(x, y, w, h);
   }
 
   update(dT, ground) {
