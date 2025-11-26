8675df697b3a0169e103c1a0a76303fde4e575ae
IsakA <ial0705@student.skelleftea.se>
2025-11-26 09:14:03 +0100

    zombie hp and bullet death

--- src/Zombie.js
+++ src/Zombie.js
@@ -3,9 +3,10 @@ import Entity from "./Entity";
 export default class Zombie extends Entity {
   constructor(x, y, w, h, sprite = null) {
     super(x, y, w, h, sprite);
-    this.x_movement_speed = 0.1;
+    this.x_movement_speed = random(0.09, 0.2);
     this.dir = round(random(0, 1)) * 2 - 1;
     this.dead = false;
+    this.hp = round(random(1, 3));
   }
   update(dT, bullets) {
     if (this.x > width - this.w) {
@@ -18,9 +19,11 @@ export default class Zombie extends Entity {
     }
     for (let bullet of bullets) {
       if (this.hitbox.intersectsWith(bullet.hitbox)) {
-        this.dead = true;
+        this.hp--;
+        bullet.dead = true;
       }
     }
+    this.dead = this.hp <= 0;
     this.x_vel = this.dir * this.x_movement_speed * dT;
 
     this.x += this.x_vel;
